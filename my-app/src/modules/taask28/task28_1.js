675f42ecb0a44fcb521f83e8b95dd89e1720388d
Daniyar Bashirov <tehnikdaniyar@gmail.com>
2023-03-30 00:03:54 +0600

    decided task28_1

--- my-app/src/modules/taask28/task28_1.js
+++ my-app/src/modules/taask28/task28_1.js
@@ -1,13 +1,24 @@
 import { useState } from "react";
 const Task28_1 = function () {
 	const [numbers, setNumbers] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+	console.log(numbers);
 
 	function getAverage(arr) {
-		return arr.reduce((summ, num) => summ + num) / arr.length;
+		// return arr.reduce((summ, num) => summ + num) / arr.length;
+		let sum = 0;
+		for (let num of arr) {
+			if (num) {
+				sum += num;
+			};
+		};
+		return sum;
 	};
 
 	function handlerChange(index, event) {
-		setNumbers([...numbers.slice(0, index)], event.target.value, ...numbers.slice(index + 1))
+		console.log(index);
+		console.log(event.target.value);
+		console.log();
+		setNumbers([...numbers.slice(0, index), Number(event.target.value), ...numbers.slice(index + 1)])
 	}
 
 	const inputs = numbers.map((number, index) => <input key={index} type="text" value={number} onChange={(event) => handlerChange(index, event)}></input>)
